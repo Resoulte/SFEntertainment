87271341df3b9d2a2035c48c03613dd9e77be5fc
Resolute <sferic@163.com>
2016-08-27 15:51:13 +0800

    初始化项目

---
+++ BSBDJ/BSBDJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBDJ
+//
+//  Created by ma c on 16/8/27.
+//  Copyright © 2016年 shifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
